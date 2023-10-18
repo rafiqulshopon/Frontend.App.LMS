beea7adc10c0c53a4c728279c7a3a3da96a94694
Rafiqul Shopon <rafiqul.dev@gmail.com>
2023-10-19 02:54:56 +0600

    implement login api call

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,6 +1,32 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import axios from '../../axios';
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    setError(null);
+
+    try {
+      const response = await axios.post('/auth/login', {
+        email: email,
+        password: password,
+      });
+
+      const token = response.data.token;
+
+      localStorage.setItem('accessToken', token);
+    } catch (err) {
+      setError(
+        err.response?.data?.message || 'An error occurred during login.'
+      );
+    }
+  };
+
   return (
     <section className='bg-cyan-50	'>
       <div className='flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0'>
@@ -9,7 +35,13 @@ const Login = () => {
             <h1 className='text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl'>
               Sign in to your account
             </h1>
-            <form className='space-y-4 md:space-y-6' action='#'>
+            <form
+              className='space-y-4 md:space-y-6'
+              action='#'
+              onSubmit={handleLogin}
+            >
+              {error && <div className='text-red-500'>{error}</div>}
+
               <div>
                 <label
                   htmlFor='email'
@@ -21,6 +53,7 @@ const Login = () => {
                   type='email'
                   name='email'
                   id='email'
+                  onChange={(e) => setEmail(e.target.value)}
                   className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5'
                   placeholder='Enter your email'
                   required
@@ -38,6 +71,7 @@ const Login = () => {
                   name='password'
                   id='password'
                   placeholder='••••••••'
+                  onChange={(e) => setPassword(e.target.value)}
                   className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5'
                   required
                 />
