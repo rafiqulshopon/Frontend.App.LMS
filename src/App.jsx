daf366ad5664a9b16486fbe4799b89673fc4ff6e
Rafiqul Shopon <rafiqul.dev@gmail.com>
2023-11-02 23:11:57 +0600

    implemented user logged in check

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import HomePage from '@/pages/HomePage.jsx';
 import Login from '@/pages/login';
@@ -12,41 +13,47 @@ import Books from './pages/books';
 import Reservations from './pages/reservations';
 import BorrowReturn from './pages/borrow-return';
 import Settings from './pages/settings';
+import Cookies from 'js-cookie';
+import { AuthContext } from './context/AuthContext';
 
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(!!Cookies.get('accessToken'));
+
   const routes = [
     { path: '/', component: <HomePage />, show: true },
     { path: '/login', component: <Login />, show: true },
     { path: '/signup', component: <Signup />, show: true },
     { path: '/forget-password', component: <ForgetPassword />, show: true },
     { path: '/verify-otp', component: <VerifyOTP />, show: true },
-    { path: '/dashboard', component: <Dashboard />, show: true },
-    { path: '/users', component: <Users />, show: true },
-    { path: '/books', component: <Books />, show: true },
-    { path: '/reservations', component: <Reservations />, show: true },
-    { path: '/borrow-return', component: <BorrowReturn />, show: true },
-    { path: '/settings', component: <Settings />, show: true },
+    { path: '/dashboard', component: <Dashboard />, show: isLoggedIn },
+    { path: '/users', component: <Users />, show: isLoggedIn },
+    { path: '/books', component: <Books />, show: isLoggedIn },
+    { path: '/reservations', component: <Reservations />, show: isLoggedIn },
+    { path: '/borrow-return', component: <BorrowReturn />, show: isLoggedIn },
+    { path: '/settings', component: <Settings />, show: isLoggedIn },
   ];
 
   return (
-    <BrowserRouter>
-      <Layout>
-        <Routes>
-          {routes.map(
-            (route, index) =>
-              route.show && (
-                <Route
-                  key={index}
-                  path={route.path}
-                  element={route.component}
-                />
-              )
-          )}
-          <Route path='/404' element={<NotFoundPage />} />
-          <Route path='*' element={<Navigate to='/404' replace />} />
-        </Routes>
-      </Layout>
-    </BrowserRouter>
+    <AuthContext.Provider value={{ isLoggedIn, setIsLoggedIn }}>
+      <BrowserRouter>
+        <Layout>
+          <Routes>
+            {routes.map(
+              (route, index) =>
+                route.show && (
+                  <Route
+                    key={index}
+                    path={route.path}
+                    element={route.component}
+                  />
+                )
+            )}
+            <Route path='/404' element={<NotFoundPage />} />
+            <Route path='*' element={<Navigate to='/404' replace />} />
+          </Routes>
+        </Layout>
+      </BrowserRouter>
+    </AuthContext.Provider>
   );
 };
 
