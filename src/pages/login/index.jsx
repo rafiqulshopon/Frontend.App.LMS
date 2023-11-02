b76bad98a9c0d7fb013b76e6f5d9482410dd962f
Rafiqul Shopon <rafiqul.dev@gmail.com>
2023-11-02 22:17:42 +0600

    update login component

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,19 +1,21 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import axios from '../../axios';
-import { Form, Input, Button, Checkbox } from 'antd';
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const [error, setError] = useState(null);
   const navigate = useNavigate();
 
-  const handleLogin = async (values) => {
+  const handleLogin = async (e) => {
+    e.preventDefault();
     setError(null);
 
     try {
       const response = await axios.post('/auth/login', {
-        email: values.email,
-        password: values.password,
+        email: email,
+        password: password,
       });
 
       const token = response.data.token;
@@ -28,60 +30,98 @@ const Login = () => {
   };
 
   return (
-    <div className='bg-blue-100 h-screen flex items-center justify-center'>
-      <div className='bg-white p-10 rounded-lg shadow-md w-96'>
-        <p className='text-center text-xl font-medium mb-6'>
-          Sign in to your account
-        </p>
-
-        <Form layout='vertical' onFinish={handleLogin}>
-          {error && <div className='text-red-500 mb-4'>{error}</div>}
-
-          <Form.Item
-            label={<span className='font-semibold'>Your email</span>}
-            name='email'
-            rules={[{ required: true, message: 'Please input your email!' }]}
-          >
-            <Input placeholder='Enter your email' />
-          </Form.Item>
-
-          <Form.Item
-            label={<span className='font-semibold'>Password</span>}
-            name='password'
-            rules={[{ required: true, message: 'Please input your password!' }]}
-          >
-            <Input.Password placeholder='••••••••' />
-          </Form.Item>
-
-          <Form.Item className='mb-6'>
-            <Form.Item name='remember' valuePropName='checked' noStyle>
-              <Checkbox>Remember me</Checkbox>
-            </Form.Item>
-            <Link to='/forget-password' className='float-right'>
-              Forgot password?
-            </Link>
-          </Form.Item>
-
-          <Form.Item>
-            <Button
-              type='primary'
-              htmlType='submit'
-              block
-              className='bg-blue-600 hover:bg-blue-700'
+    <section className='bg-cyan-50	'>
+      <div className='flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0'>
+        <div className='w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0'>
+          <div className='p-6 space-y-4 md:space-y-6 sm:p-8'>
+            <h1 className='text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl'>
+              Sign in to your account
+            </h1>
+            <form
+              className='space-y-4 md:space-y-6'
+              action='#'
+              onSubmit={handleLogin}
             >
-              Sign in
-            </Button>
-          </Form.Item>
+              {error && <div className='text-red-500'>{error}</div>}
 
-          <p className='text-center'>
-            Don’t have an account yet?{' '}
-            <Link to='/signup' className='text-blue-600'>
-              Sign up
-            </Link>
-          </p>
-        </Form>
+              <div>
+                <label
+                  htmlFor='email'
+                  className='block mb-2 text-sm font-medium text-gray-900'
+                >
+                  Your email
+                </label>
+                <input
+                  type='email'
+                  name='email'
+                  id='email'
+                  onChange={(e) => setEmail(e.target.value)}
+                  className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5'
+                  placeholder='Enter your email'
+                  required
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor='password'
+                  className='block mb-2 text-sm font-medium text-gray-900'
+                >
+                  Password
+                </label>
+                <input
+                  type='password'
+                  name='password'
+                  id='password'
+                  placeholder='••••••••'
+                  onChange={(e) => setPassword(e.target.value)}
+                  className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5'
+                  required
+                />
+              </div>
+              <div className='flex items-center justify-between'>
+                <div className='flex items-start'>
+                  <div className='flex items-center h-5'>
+                    <input
+                      id='remember'
+                      aria-describedby='remember'
+                      type='checkbox'
+                      className='w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300'
+                      required
+                    />
+                  </div>
+                  <div className='ml-3 text-sm'>
+                    <label htmlFor='remember' className='text-gray-500'>
+                      Remember me
+                    </label>
+                  </div>
+                </div>
+                <Link
+                  to='/forget-password'
+                  className='text-sm font-medium text-primary-600 hover:underline'
+                >
+                  Forgot password?
+                </Link>
+              </div>
+              <button
+                type='submit'
+                className='w-full text-white bg-primary hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center'
+              >
+                Sign in
+              </button>
+              <p className='text-sm font-light text-gray-500'>
+                Don’t have an account yet?{' '}
+                <Link
+                  to='/signup'
+                  className='font-medium text-primary-600 hover:underline'
+                >
+                  Sign up
+                </Link>
+              </p>
+            </form>
+          </div>
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
 
