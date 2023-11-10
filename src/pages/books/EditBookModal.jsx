1b8c32bd05661a008290953f0059affd7800af6d
Rafiqul Shopon <rafiqul.dev@gmail.com>
2023-11-11 02:43:28 +0600

    update loading

--- src/pages/books/EditBookModal.jsx
+++ src/pages/books/EditBookModal.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   Modal,
   Form,
@@ -10,6 +10,7 @@ import {
   message,
   Row,
   Col,
+  Spin,
 } from 'antd';
 import axiosInstance from '../../axios';
 import moment from 'moment';
@@ -26,6 +27,8 @@ const EditBookModal = ({
 }) => {
   const [form] = Form.useForm();
 
+  const [loading, setLoading] = useState(true);
+
   const fetchBookDetails = async () => {
     try {
       const response = await axiosInstance.get(`/book/${bookId}`);
@@ -34,9 +37,11 @@ const EditBookModal = ({
         ...bookDetails,
         publishedDate: moment(bookDetails.publishedDate),
       });
+      setLoading(false);
     } catch (error) {
       message.error('Failed to fetch book details');
       console.error('Error fetching book details:', error);
+      setLoading(false);
     }
   };
 
@@ -73,132 +78,143 @@ const EditBookModal = ({
       width={650}
       footer={null}
     >
-      <Form form={form} layout='vertical' onFinish={onFinish}>
-        <Row gutter={16}>
-          <Col span={12}>
-            <Form.Item
-              name='title'
-              label='Title'
-              rules={[{ required: true, message: 'Please input the title!' }]}
-            >
-              <Input placeholder='Enter book title' />
-            </Form.Item>
-          </Col>
-          <Col span={12}>
-            <Form.Item
-              name='author'
-              label='Author'
-              rules={[{ required: true, message: 'Please input the author!' }]}
-            >
-              <Input placeholder='Enter author name' />
-            </Form.Item>
-          </Col>
-        </Row>
+      {!loading ? (
+        <Form form={form} layout='vertical' onFinish={onFinish}>
+          <Row gutter={16}>
+            <Col span={12}>
+              <Form.Item
+                name='title'
+                label='Title'
+                rules={[{ required: true, message: 'Please input the title!' }]}
+              >
+                <Input placeholder='Enter book title' />
+              </Form.Item>
+            </Col>
+            <Col span={12}>
+              <Form.Item
+                name='author'
+                label='Author'
+                rules={[
+                  { required: true, message: 'Please input the author!' },
+                ]}
+              >
+                <Input placeholder='Enter author name' />
+              </Form.Item>
+            </Col>
+          </Row>
 
-        <Row gutter={16}>
-          <Col span={12}>
-            <Form.Item
-              name='department'
-              label='Department'
-              rules={[
-                { required: true, message: 'Please select the department!' },
-              ]}
-            >
-              <Select placeholder='Select department'>
-                <Option value='CSE'>CSE</Option>
-                <Option value='LHR'>LHR</Option>
-                <Option value='PHR'>PHR</Option>
-                <Option value='ENG'>ENG</Option>
-              </Select>
-            </Form.Item>
-          </Col>
-          <Col span={12}>
-            <Form.Item
-              name='publishedDate'
-              label='Published Date'
-              rules={[
-                {
-                  required: true,
-                  message: 'Please select the published date!',
-                },
-              ]}
-            >
-              <DatePicker style={{ width: '100%' }} />
-            </Form.Item>
-          </Col>
-        </Row>
+          <Row gutter={16}>
+            <Col span={12}>
+              <Form.Item
+                name='department'
+                label='Department'
+                rules={[
+                  { required: true, message: 'Please select the department!' },
+                ]}
+              >
+                <Select placeholder='Select department'>
+                  <Option value='CSE'>CSE</Option>
+                  <Option value='LHR'>LHR</Option>
+                  <Option value='PHR'>PHR</Option>
+                  <Option value='ENG'>ENG</Option>
+                </Select>
+              </Form.Item>
+            </Col>
+            <Col span={12}>
+              <Form.Item
+                name='publishedDate'
+                label='Published Date'
+                rules={[
+                  {
+                    required: true,
+                    message: 'Please select the published date!',
+                  },
+                ]}
+              >
+                <DatePicker style={{ width: '100%' }} />
+              </Form.Item>
+            </Col>
+          </Row>
 
-        <Row gutter={16}>
-          <Col span={12}>
-            <Form.Item
-              name='category'
-              label='Category'
-              rules={[
-                { required: true, message: 'Please input the category!' },
-              ]}
-            >
-              <Input placeholder='Enter book category' />
-            </Form.Item>
-          </Col>
-          <Col span={12}>
-            <Form.Item
-              name='isbn'
-              label='ISBN'
-              rules={[{ required: true, message: 'Please input the ISBN!' }]}
-            >
-              <Input placeholder='Enter book ISBN' />
-            </Form.Item>
-          </Col>
-        </Row>
+          <Row gutter={16}>
+            <Col span={12}>
+              <Form.Item
+                name='category'
+                label='Category'
+                rules={[
+                  { required: true, message: 'Please input the category!' },
+                ]}
+              >
+                <Input placeholder='Enter book category' />
+              </Form.Item>
+            </Col>
+            <Col span={12}>
+              <Form.Item
+                name='isbn'
+                label='ISBN'
+                rules={[{ required: true, message: 'Please input the ISBN!' }]}
+              >
+                <Input placeholder='Enter book ISBN' />
+              </Form.Item>
+            </Col>
+          </Row>
 
-        <Form.Item name='description' label='Description'>
-          <TextArea placeholder='Enter book description' />
-        </Form.Item>
+          <Form.Item name='description' label='Description'>
+            <TextArea placeholder='Enter book description' />
+          </Form.Item>
 
-        <Row gutter={16}>
-          <Col span={12}>
-            <Form.Item
-              name='totalQuantity'
-              label='Total Quantity'
-              rules={[
-                { required: true, message: 'Please input the total quantity!' },
-              ]}
-            >
-              <InputNumber min={1} style={{ width: '100%' }} />
-            </Form.Item>
-          </Col>
-          <Col span={12}>
-            <Form.Item
-              name='currentQuantity'
-              label='Current Quantity'
-              rules={[
-                {
-                  required: true,
-                  message: 'Please input the current quantity!',
-                },
-              ]}
-            >
-              <InputNumber min={0} style={{ width: '100%' }} />
-            </Form.Item>
-          </Col>
-        </Row>
+          <Row gutter={16}>
+            <Col span={12}>
+              <Form.Item
+                name='totalQuantity'
+                label='Total Quantity'
+                rules={[
+                  {
+                    required: true,
+                    message: 'Please input the total quantity!',
+                  },
+                ]}
+              >
+                <InputNumber min={1} style={{ width: '100%' }} />
+              </Form.Item>
+            </Col>
+            <Col span={12}>
+              <Form.Item
+                name='currentQuantity'
+                label='Current Quantity'
+                rules={[
+                  {
+                    required: true,
+                    message: 'Please input the current quantity!',
+                  },
+                ]}
+              >
+                <InputNumber min={0} style={{ width: '100%' }} />
+              </Form.Item>
+            </Col>
+          </Row>
 
-        <Form.Item>
-          <div style={{ textAlign: 'right' }}>
-            <Button key='back' onClick={handleCancel}>
-              Cancel
-            </Button>
-            <Button
-              key='submit'
-              type='primary'
-              htmlType='submit'
-              className='bg-blue-500 hover:bg-blue-700 ml-2'
-            >
-              Update Book
-            </Button>
-          </div>
-        </Form.Item>
-      </Form>
+          <Form.Item>
+            <div style={{ textAlign: 'right' }}>
+              <Button key='back' onClick={handleCancel}>
+                Cancel
+              </Button>
+              <Button
+                key='submit'
+                type='primary'
+                htmlType='submit'
+                className='bg-blue-500 hover:bg-blue-700 ml-2'
+              >
+                Update Book
+              </Button>
+            </div>
+          </Form.Item>
+        </Form>
+      ) : (
+        <div className='flex justify-center items-center mt-20 mb-20'>
+          <Spin />
+        </div>
+      )}
     </Modal>
   );
 };
