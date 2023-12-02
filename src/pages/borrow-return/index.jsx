298821df6f925948a14ab5d5f0266988b420f73f
Rafiqul Shopon <rafiqul.dev@gmail.com>
2023-12-02 15:58:26 +0600

    implement borrow list

--- src/pages/borrow-return/index.jsx
+++ src/pages/borrow-return/index.jsx
@@ -1,5 +1,119 @@
+import React, { useEffect, useState } from 'react';
+import { Table, message, Space, Input, Button } from 'antd';
+import { EllipsisOutlined, SearchOutlined } from '@ant-design/icons';
+import axiosInstance from '../../axios';
+import AppFilterRadio from '../../helpers/ui/radio/AppFilterRadio';
+
 const BorrowReturn = () => {
-  return <div className='mt-4 mr-4 ml-4'>BorrowReturn</div>;
+  const [borrowingHistories, setBorrowingHistories] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchBorrowingHistories = async () => {
+    try {
+      const response = await axiosInstance.post('/borrow-list');
+      setBorrowingHistories(response.data.borrowingHistories || []);
+    } catch (error) {
+      message.error('Error fetching borrowing histories');
+      console.error('Error:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchBorrowingHistories();
+  }, []);
+
+  const columns = [
+    {
+      title: 'Book Title',
+      dataIndex: ['book', 'title'],
+      key: 'bookTitle',
+    },
+    {
+      title: 'Borrower',
+      key: 'borrower',
+      render: (_, record) =>
+        `${record.user.name.first} ${record.user.name.last}`,
+    },
+    {
+      title: 'Borrow Date',
+      dataIndex: 'borrowDate',
+      key: 'borrowDate',
+      render: (borrowDate) => new Date(borrowDate).toLocaleDateString(),
+    },
+    {
+      title: 'Expected Return Date',
+      dataIndex: 'expectedReturnDate',
+      key: 'expectedReturnDate',
+      render: (expectedReturnDate) =>
+        new Date(expectedReturnDate).toLocaleDateString(),
+    },
+    {
+      title: 'Status',
+      dataIndex: 'status',
+      key: 'status',
+    },
+    {
+      title: 'Actions',
+      key: 'actions',
+      render: () => (
+        <Space>
+          <a onClick={(e) => e.preventDefault()}>
+            <EllipsisOutlined />
+          </a>
+        </Space>
+      ),
+    },
+  ];
+
+  const departmentOptions = [
+    { label: 'CSE', value: 'CSE' },
+    { label: 'LHR', value: 'LHR' },
+    { label: 'PHR', value: 'PHR' },
+    { label: 'ENG', value: 'ENG' },
+  ];
+
+  const handleSelectionChange = (value) => {
+    // setLoading(true);
+  };
+
+  return (
+    <div className='mt-4 mx-4 bg-white p-6 rounded-lg shadow'>
+      <div className='flex justify-between items-center mb-4'>
+        <div className='flex gap-4 flex-grow'>
+          <Input
+            placeholder='Search books'
+            className='w-1/4'
+            prefix={<SearchOutlined />}
+            // value={searchKeyword}
+            // onChange={handleSearchChange}
+          />
+
+          <AppFilterRadio
+            options={departmentOptions}
+            onChange={handleSelectionChange}
+            btn_text='Department'
+          />
+        </div>
+
+        <Button
+          type='primary'
+          className='bg-blue-500 hover:bg-blue-700 text-white'
+        >
+          Assign Book
+        </Button>
+      </div>
+      <Table
+        dataSource={borrowingHistories}
+        columns={columns}
+        rowKey='_id'
+        pagination={false}
+        loading={loading}
+        className='rounded-lg overflow-hidden'
+      />
+    </div>
+  );
 };
 
 export default BorrowReturn;
